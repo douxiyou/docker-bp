3d1c03fc771dcb431af3ec4e5e1a27ee33161370
GitHub Action <action@github.com>
2022-06-13 05:12:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/douxiyou/dockerbp/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/douxiyou/dockerbp/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.douxiyou.dockerbp.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.douxiyou.dockerbp.MyBundle
 
 class MyApplicationService {
 
